ac2ca77dfa410a802892f4d9453612b3eb9078fa
Žiga <ziga.ring@gmail.com>
2023-05-06 21:32:22 +0200

    second commit

--- src/EventDayCounter.php
+++ src/EventDayCounter.php
@@ -3,16 +3,23 @@
 namespace Drupal\event_day_counter;
 
 class EventDayCounter {
-  public function getDaysDiff($event_date) {
+    public function getDaysDiff($event_date) {
 
-    $event_time = strtotime($event_date);
-    $current_time = time();
+      $event_time = strtotime($event_date); 
+      $current_time = time(); 
 
-    $current_day_start = strtotime('today', $current_time);
-    $event_day_start = strtotime('today', $event_time);
+      $event_hour = date('H', $event_time);  //check for hour of event
 
-    $days_diff = round(($event_day_start - $current_day_start) / (24 * 60 * 60));
-    return $days_diff;
-    }
+      $current_day_start = strtotime('today', $current_time);
+      
+      if($event_hour >= 22){
+        $event_day_start = strtotime('+2 hours', $event_time); 
+      }else{
+        $event_day_start = strtotime('today', $event_time);
+      }
+
+      $days_diff = round(($event_day_start - $current_day_start) / (24 * 60 * 60));
+      return $days_diff;
+      }
 }
 
